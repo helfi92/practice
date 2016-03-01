c3102dededa7599f9e8298fcd57ca0a76daae88b
Hassan Ali <helfi92@gmail.com>
2016-02-29 19:09:40 -0500

    add minute marks

--- animated-clock/animated-clock.js
+++ animated-clock/animated-clock.js
@@ -7,14 +7,14 @@ console.log('boom');
 
 		ctx.translate(75,75);
 		ctx.scale(0.4,0.4);
-
+		ctx.rotate(-Math.PI/2);
 		ctx.strokeStyle = "black";
 		ctx.fillStyle = "white";
 
 
 		ctx.save();
 
-		//hours
+		//hours marks
 		for(var i = 0 ; i < 12 ; i++){
 			ctx.beginPath();
 			ctx.rotate(Math.PI/6);
@@ -24,10 +24,23 @@ console.log('boom');
 		}
 
 		ctx.restore();
-		//minutes
+		//minutes marks
 		for(var i = 0 ; i < 60 ; i++){
+			
+
+			//draw if does not intersect hours marks
+			if(i % 5 != 0){
+				ctx.beginPath();
+				ctx.moveTo(117,0);
+				ctx.lineTo(120,0);
+				ctx.stroke();	
+			}
+
+			ctx.rotate(Math.PI/30);
 
 		}
+
+		ctx.restore();
 	}
 
 
